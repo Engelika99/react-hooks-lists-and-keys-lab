da0b7d56ad007e48f2936c5a83b5e7c60d8d20b2
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-05-22 22:26:45 -0400

    Displays list and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
+/** @format */
+
 import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link, array) => (
+        <a key={array} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
